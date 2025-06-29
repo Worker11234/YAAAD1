dced5ba35021368ca0537c822bb80e675fbf52b6
Worker11234 <t55434407@gmail.com>
2025-06-29 22:36:36 +0530

    Updated dock-two.tsx

--- src/components/ui/dock-two.tsx
+++ src/components/ui/dock-two.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { motion } from "framer-motion"
 import { cn } from "../../lib/utils"
-import { LucideIcon } from "lucide-react"
+import { DivideIcon as LucideIcon } from "lucide-react"
 
 interface DockProps {
   className?: string
